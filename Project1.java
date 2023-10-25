ff549e26cda20786061bbdcc06dd3925d7594e3f
JuliusSims <jsims1@student.framingham.edu>
2023-10-25 13:18:12 -0400

    updated Project1

--- Project1.java
+++ Project1.java
@@ -2,9 +2,11 @@ class Project1
 {
     public static void main(String args[])
     {
-        int x = 2;
+        int x = 4;
         int y = 3;
         
+        System.out.println("The sum is " + (x+y));
+        System.out.println("The difference is " + (x-y));
 
     }
 }
